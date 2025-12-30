2b589f3724884efe16a3f158cdba22a29b3901ab
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-12-29 22:03:49 -0600

    add react router and tank stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { RouterProvider } from "react-router/dom";
+import { router } from "./routes/AppRouter";
+
 
 function App() {
   return (
-    <main className="container">
-      <Button>Click me</Button>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
