a95ffecaedd3efeb9bec7b548376fa5b810d5c32
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-12-29 21:53:21 -0600

    shadcn and tailwind added to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <main className="container">
-      
+      <Button>Click me</Button>
     </main>
   );
 }
