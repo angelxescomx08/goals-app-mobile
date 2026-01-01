5aee4442c41abbe049055ed80ac6c45b5b3b2326
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2026-01-01 10:40:08 -0600

    header data

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -17,15 +17,12 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-
-// Mock de datos basado en tu tabla 'user'
-const userData = {
-  name: "Usuario Ejemplo",
-  email: "usuario@ejemplo.com",
-  image: "https://github.com/shadcn.png",
-};
+import { authClient } from "@/lib/auth";
 
 export function MainHeader() {
+
+  const { data: session } = authClient.useSession();
+
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container flex h-16 items-center justify-between px-4 md:px-8">
@@ -72,16 +69,16 @@ export function MainHeader() {
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-10 w-10 rounded-full">
                 <Avatar className="h-10 w-10 border">
-                  <AvatarImage src={userData.image} alt={userData.name} />
-                  <AvatarFallback>{userData.name.charAt(0)}</AvatarFallback>
+                  <AvatarImage src={session?.user?.image ?? undefined} alt={session?.user?.name ?? undefined} />
+                  <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" align="end" forceMount>
               <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">{userData.name}</p>
-                  <p className="text-xs leading-none text-muted-foreground">{userData.email}</p>
+                  <p className="text-sm font-medium leading-none">{session?.user?.name ?? undefined}</p>
+                  <p className="text-xs leading-none text-muted-foreground">{session?.user?.email ?? undefined}</p>
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
